654dd6a69be678ce47abac9230e0ac5603c69aea
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-01 14:15:56 -0700

    fix(hintLog): update hintLog dependency

--- hint-modules_test.js
+++ hint-modules_test.js
@@ -18,17 +18,17 @@ describe('hintModules', function() {
   it('should identify modules created and not loaded', function() {
     angular.module('createdAndNotLoaded', []);
     start();
-    expect(Object.keys(hintLog.flush()['Modules'])[0]).toBe(' Module "createdAndNotLoaded" was created but never loaded.');
+    expect(Object.keys(hintLog.flush()['Modules'])[0]).toBe('Module "createdAndNotLoaded" was created but never loaded.');
   });
   it('should identify modules loaded that do not exsist', function() {
     angular.module('testModule', ['doesntExist']);
     start();
-    expect(Object.keys(hintLog.flush()['Modules'])[1]).toBe(' Module "doesntExist" was loaded but does not exist.');
+    expect(Object.keys(hintLog.flush()['Modules'])[1]).toBe('Module "doesntExist" was loaded but does not exist.');
   });
   it('should identify modules that have been loaded multiple times', function() {
     angular.module('testModule', []);
     start();
-    expect(Object.keys(hintLog.flush()['Modules'])[2]).toBe(' Multiple modules with name "testModule" are being created and they will overwrite each other.');
+    expect(Object.keys(hintLog.flush()['Modules'])[2]).toBe('Multiple modules with name "testModule" are being created and they will overwrite each other.');
   });
   it('should ignore modules loaded twice if one is just being called', function() {
     angular.module('testModule2', []);
@@ -36,7 +36,7 @@ describe('hintModules', function() {
     start();
     var results = Object.keys(hintLog.flush()['Modules']);
     var finResult = results.some(function(res){
-      return res === " Multiple modules with name 'testModule' are being created and they will overwrite each other.";
+      return res === "Multiple modules with name 'testModule' are being created and they will overwrite each other.";
     });
     expect(finResult).toBe(false);
   });
