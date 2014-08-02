a8c98aa89ff173649c2bf36608e829af1692c7ee
Erin Altenhof-Long <ealtenhof@google.com>
2014-08-01 17:13:12 -0700

    feat(getNgAppMod): warn about using multiple ng-apps
    When modules are being created, check if they are the `ng-app` module.
    If multiple `ng-app` modules are found, warn that only one will
    be used to bootstrap.
    
    Closes #9

--- test/getNgAppMod_test.js
+++ test/getNgAppMod_test.js
@@ -1,4 +1,5 @@
 var getNgAppMod = require('../lib/getNgAppMod');
+var hintLog = angular.hint;
 
 describe('getNgAppMod()', function() {
   it('should return the value of the ng-app attribute', function() {
@@ -9,5 +10,18 @@ describe('getNgAppMod()', function() {
     };
     var res = getNgAppMod(attributes);
     expect(res).toBe('testModule');
+  });
+
+  it('should log if multiple ng-apps are detected', function() {
+    var attributes = {
+        'width': {value: '10px'},
+        'id': {value: 'idName'},
+        'ng-app': {value: 'testModule'}
+    };
+    var foundNgApp = getNgAppMod(attributes, false);
+    getNgAppMod(attributes, foundNgApp);
+    var log = hintLog.flush();
+    expect(Object.keys(log['Modules'])).toEqual(['ng-app may only be included once. The module ' +
+      '"testModule" was not used to bootstrap because ng-app was already included.']);
   });
 });
